c94fa5fa3af6e77999134f3bca7215dd83ea508f
mirackarakasli <ilhansamikara@gmail.com>
2021-04-24 00:27:40 +0300

    isim düzenlemesi ve Armstrong number eklenmesi

---
+++ Python_Task/Armstrong_number.py
@@ -0,0 +1,25 @@
+sayi = input("Enter positive number\n")
+a = len(sayi)
+liste = []
+
+if sayi.isnumeric() == True:
+    for i in sayi:
+        liste.append(int(i)**a)
+
+    b = sum(liste)
+
+    if int(sayi) == b:
+        print(sayi, "is an Armsrong number")
+    else:
+        print(sayi, "is not an Armstrong number")
+    
+else:
+    print(" It is an invalid entry. Don't use non-numeric, float, or negative values!")
+    
+
+
+
+
+
+
+
---
+++ Python_Task/Password_Reminder.py
@@ -0,0 +1,7 @@
+first_name = input("Enter First Name : ").title()
+user_name = "Joseph"
+
+if first_name == user_name:
+	print("Hello, Joseph! The password is : W@12")
+else:
+	print(f"Hello, {first_name}! See you later")
