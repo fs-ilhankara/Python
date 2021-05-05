11435335d93b1a8ace7e5c17aa79abab92e2bed2
mirackarakasli <ilhansamikara@gmail.com>
2021-05-05 23:15:23 +0300

    fizzbuzz düzenleme

--- Python_Task/FizzBuzz.py
+++ Python_Task/FizzBuzz.py
@@ -1,16 +1,11 @@
 def fizzbuzz():
-    liste = []
     for i in range(1,101):
         if i%3 == 0 and i%5 == 0:
-            i = "FizzBuzz"
-            liste.append(i)
+            print("FizzBuzz")            
         elif i%3 ==0:
-            i = "Fizz"
-            liste.append(i)
+            print("Fizz")
         elif i%5 == 0:
-            i = "Buzz"
-            liste.append(i)
-        else:
-            liste.append(i)
-    print(liste)
-fizzbuzz()        
+            print("Buzz")
+        else:            
+            print(i)
+fizzbuzz() 
